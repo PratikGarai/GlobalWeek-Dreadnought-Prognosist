0f0e84e99dc4a1e87c30dbe14047f544aaebaea7
Pratik Garai <pratikgarai0208@gmail.com>
2020-06-10 19:26:36 +0530

    Login and logout fully functional

--- DiseasePredictor/Doctor/urls.py
+++ DiseasePredictor/Doctor/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'Doctor'
 
 urlpatterns = [
-        url('^register/$', views.register, name="Registration"),
+        url('^register/$', views.register , name="Registration"),
+        url('^login/$', views.login_user , name="Login"),
+        url('^logout/$', views.logout_user , name="Logout"),
         ]
--- DiseasePredictor/Doctor/views.py
+++ DiseasePredictor/Doctor/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from . import forms
 
 def register(request):
@@ -28,8 +30,32 @@ def register(request):
             # for now, I make registered true for testing
             doctor.save()
             registered = True
-            return render(request, "registration_success.html", {})
+            return render(request, "message.html", {'message': "You have successfully registered", 'header': "Registration Success"})
+        return render(request, "something_wrong.html",{}) 
+    
+    return render(request, "registration.html", {'User':forms.UserForm ,'Doctor':forms.Single_UserForm })
+
+
+def login_user(request):
+    
+    if request.method=="POST":
+        if True:
+            username = request.POST.get('username')    
+            password = request.POST.get('password')
+
+            user = authenticate(username=username , password=password)
+
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    return render(request, "message.html", {'message': "You have Successfully logged in!", 'header': "Login Succes"})
+                return render(request, "message.html", {'message': "Your account is Inactive!", 'header': "Inactive"})
+            return render(request, "login.html", {'log': forms.UserForm, 'Correct': False , 'message':''})
         return render(request, "something_wrong.html",{}) 
 
-    else:
-        return render(request, "registration.html", {'User':forms.UserForm ,'Doctor':forms.Single_UserForm })
+    return render(request, "login.html", { 'log' : forms.UserForm , 'Correct' : True, 'message' : "Fill the form to Login"})
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return render(request, "login.html", { 'log' : forms.UserForm, 'Correct' : True, 'message': "You logged out"})
