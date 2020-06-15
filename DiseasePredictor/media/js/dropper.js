caa7aa57c2b80340e8282d51a678c4ddbe81b081
Pratik Garai <pratikgarai0208@gmail.com>
2020-06-15 14:15:49 +0530

    no drop added

--- DiseasePredictor/media/js/dropper.js
+++ DiseasePredictor/media/js/dropper.js
@@ -1,40 +1,49 @@
-var max_n = 0;
+var max_n = 7;
 $(document).ready(function(){
-	$("#submit").click(function(event){
-		var box = $(".main-div div#selections div");
-		var inputs = $("input.field");
-		for( var i=0;i<box.length;i++)
-		{
-			inputs[i].value = box[i].innerText;
-		}
-		$("#target-form").submit()
-	});
-	max_n = $(".field").length;
+   $("#submit").click(function(event){
+      var box = $(".main-div div#selections div");
+      var inputs = $("input.field");
+      for( var i=0;i<box.length;i++)
+      {
+         inputs[i].value = box[i].innerText;
+      }
+      $("#target-form").submit()
+   });
+   max_n = $(".field").length;
 });
 var count = 0;
 function allowDrop(ev) {
-	ev.preventDefault();
+   ev.preventDefault();
 }
-
 function drag(ev) {
-	ev.dataTransfer.setData("text", ev.target.id);
+   ev.dataTransfer.setData("text", ev.target.id);
 }
-
 function drop1(ev) {
-	if(count===max_n)
-	{
-		alert ("All "+ max_n+ " elements selected");
-		return ;
-	}
-	ev.preventDefault();
-	var data = ev.dataTransfer.getData("text");
-	ev.target.appendChild(document.getElementById(data));
+   if(count===max_n)
+   {
+      alert ("All "+ max_n+ " elements selected");
+      return ;
+   }
+   var _target = $("#" + ev.target.id);
+   var data = ev.dataTransfer.getData("text");
+     if ($(_target).hasClass("noDrop")) {
+        console.log("no transfer");
+        ev.preventDefault();
+      } else {
+        ev.preventDefault();
+        ev.target.appendChild(document.getElementById(data));
 	count++;
+      }
 }
-
 function drop2(ev) {
-	ev.preventDefault();
-	var data = ev.dataTransfer.getData("text");
-	ev.target.appendChild(document.getElementById(data));
-	count--;
+  var _target = $("#" + ev.target.id);
+  var data = ev.dataTransfer.getData("text");
+  if ($(_target).hasClass("noDrop")) {
+    console.log("no transfer");
+    ev.preventDefault();
+  } else {
+    ev.preventDefault();
+    ev.target.appendChild(document.getElementById(data));
+    count--;
+  }
 }
